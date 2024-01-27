a90f73ba5e7649767cef82eecf09d3c85f25ed68
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-01-27 16:14:55 +0100

    implementing the creation and updating of the Journal Entries

---
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Editor from '@/components/Editor'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntry = async (id: string) => {
+  const user = await getUserByClerkId()
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      compoundId: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return entry
+}
+
+const EntryPage = async ({ params }) => {
+  const entry = await getEntry(params.id)
+  return (
+    <div className="h-full w-full">
+      <Editor entry={entry} />
+    </div>
+  )
+}
+
+export default EntryPage
---
+++ utils/api.ts
@@ -0,0 +1,30 @@
+const createURL = (path: string) => {
+  return window.location.origin + path
+}
+
+export const updateEntry = async (id: string, content: string) => {
+  const res = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    })
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+}
+
+export const createNewEntry = async () => {
+  const response = await fetch(
+    new Request(createURL('/api/journal'), {
+      method: 'POST',
+    })
+  )
+
+  if (response.ok) {
+    const data = await response.json()
+    return data.data
+  }
+}
